107308fc9bd20358fe42d98317fe4c69a867cb9e
Davis Clark <141876928+davisclrk@users.noreply.github.com>
2024-03-28 18:09:54 -0400

    Update HomeFragment.java

--- app/src/main/java/com/example/project2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/project2/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.project2.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +11,17 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.project2.GPTHandler;
+import com.example.project2.R;
 import com.example.project2.databinding.FragmentHomeBinding;
+
+import java.util.concurrent.ExecutionException;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private String gptResponseText = "";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +32,27 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView gptResponse = view.findViewById(R.id.gpt_response);
+
+        if (gptResponseText.isEmpty()) {
+            GPTHandler handler = new GPTHandler();
+            try {
+                gptResponseText = handler.makeRequest().get();
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Log.i("GPTHandler", gptResponseText);
+
+            gptResponse.setText(gptResponseText);
+        } else {
+            gptResponse.setText(gptResponseText);
+        }
+
     }
 
     @Override
